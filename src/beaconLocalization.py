feda8ee9141869fcd4ad174278002590e87a99c6
Zach <zach@laptop.stanford.edu>
2019-05-09 12:48:31 -0700

    Update data types in beaconLocalization

--- src/beaconLocalization.py
+++ src/beaconLocalization.py
@@ -1,14 +1,13 @@
 import numpy as np
 import rospy
 
+from aa241x_mission.msg import SensorMeasurement, PersonEstimate
+from geometry_msgs.msg import Pose, PoseStamped
+
 
 """
-TODO: Update current subscribers (see modeController)
-TODO: Get height from pixhawk
-    May need another subscriber -> store height as its published
-TODO: Needs to publish aa241x_mission.msg.PersonEstimate (see aa241x_mission repo)
 TODO: Need to define BeaconsLocalization message    (see Zach)
-TODO: Needs to publish to /localizer/localized_beacons
+
 """
 
 
@@ -23,36 +22,47 @@ class BeaconLocalization():
         self.beacon_locations = {}      # stores beacon position and uncertainty {id : [(x,y),sigma]}
 
         ## Init Node
-        #rospy.init_node('BeaconLocalizer', anonymous=True)
+        rospy.init_node('BeaconLocalizer', anonymous=True)
 
         # Subscribers
-        #rospy.Subscriber('/beacon/position', Pose2D, self.beaconCallback, queue_size=10)
-        #rospy.Subscriber('/drone/position', Pose, self.poseCallback, queue_size=10)
+        rospy.Subscriber('/mavros/local_position/pose', PoseStamped, self.poseCallback)
+        rospy.Subscriber("/measurement", SensorMeasurement, self.beaconCallback);
 
         # Publishers
-        #self.obj_pub = rospy.Publisher('/FilteredBeaconLocations', Pose2D, queue_size=10)
-
+        # self.obj_pub = rospy.Publisher('/localizer/localized_beacons', LocalizedBeacons, queue_size=10)
+        self.person_pub = rospy.Publisher('/person_found',PersonEstimate,queue_size=10)
 
     def beaconCallback(self,msg):
         sigma = getUncertainty(self.h)
-        for beacon in msg:
-            if beacon.id in self.beacon_locations:
-                self.beacon_locations[beacon.id] = updateBeaconLocation(beacon.pos,sigma)
-            else:
-                self.beacon_locations[beacon.id] = [beacon.pos, sigma]
 
+        for i in range(msg.num_measurements):
+            # Load Data
+            id_num = msg.id[i]
+            n = msg.n[i]
+            e = msg.e[i]
+
+            # Update/Store Estimates
+            if id_num in self.beacon_locations.keys():
+                self.beacon_locations[id_num] = updateBeaconLocation(np.array([n,e]),sigma)
+            else:
+                self.beacon_locations[id_num] = [np.array([n,e]), sigma]
 
     def poseCallback(self,msg):
-        self.h = msg.position[2]
+        self.h = msg.pose.position.z
 
 
     def updateBeaconLocation(self,x,sigma):
+        # TODO: Implement Kalman Filter
         pass
 
 
 
     def publish(self):
         # TODO: publish beacon_locations
+        # Publish all beacons to person estimate
+        # Only publish beacons locations we are satisfied to localized_beacons
+        #       localized_beacons will b used by the mode_controller to determine when to swtich to localize mode,
+        #       and will be used by the navigator to determine when localization is done
         pass
 
 
